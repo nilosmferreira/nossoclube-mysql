cbb126f08601af88d780c99a79ddb4810626437b
Nilo Ferreira <nsmf.gnf@gmail.com>
2022-07-08 14:51:27 -0300

    Setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/pages/**/*.{jsx,tsx}', './src/components/**/*.{jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
